3aa35e5da213f145d870c8439a2b42a606b6165c
Aarebecca <yangtao.yangtao@alibaba-inc.com>
2021-07-19 10:34:38 +0800

    refactor(legend): 添加了title绘制

--- src/ui/legend/base.ts
+++ src/ui/legend/base.ts
@@ -1,5 +1,5 @@
 import { deepMix, get, isNumber } from '@antv/util';
-import { Rect } from '@antv/g';
+import { Rect, Text } from '@antv/g';
 import { GUI } from '../core/gui';
 import type { LegendBaseCfg, LegendBaseOptions, StyleStatus } from './types';
 
@@ -8,6 +8,8 @@ export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T>
 
   // background
   private backgroundShape: Rect;
+
+  private titleShape: Text;
 
   protected static defaultOptions = {
     type: LegendBase.tag,
@@ -26,7 +28,8 @@ export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T>
         spacing: 10,
         style: {
           fontSize: 16,
-          align: 'center',
+          align: 'left',
+          textBaseline: 'top',
         },
         formatter: (text: string) => text,
       },
@@ -39,6 +42,11 @@ export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T>
   }
 
   attributeChangedCallback(name: string, value: any) {}
+
+  public init() {
+    this.createBackground();
+    this.createTitle();
+  }
 
   // 获取对应状态的样式
   private getStyle(name: string | string[], status?: StyleStatus) {
@@ -59,13 +67,15 @@ export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T>
 
   // 获取容器内可用空间
   private getAvailableSpace() {
-    const { width, height } = this.attributes;
+    // 容器大小 - padding - title
+    const { width, height, title } = this.attributes;
+    const { fontSize: lineHeight, spacing } = title;
     const [top, right, bottom, left] = this.getPadding();
     return {
       x: left,
-      y: top,
+      y: top + lineHeight + spacing,
       width: width - (left + right),
-      height: height - (top + bottom),
+      height: height - (top + bottom) - lineHeight - spacing,
     };
   }
 
@@ -86,6 +96,44 @@ export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T>
     this.appendChild(this.backgroundShape);
   }
 
-  // 绘制标题
-  private createTitle() {}
+  /**
+   * 创建图例标题配置
+   */
+  private getTitleAttrs() {
+    const { width, title } = this.attributes;
+    const { content, style, formatter } = title;
+    const { align, ...restStyle } = style;
+
+    let layout: Object;
+    switch (align) {
+      case 'left':
+        layout = { x: 0, y: 0, textAlign: 'left' };
+        break;
+      case 'right':
+        layout = { x: width, y: 0, textAlign: 'end' };
+        break;
+      case 'center':
+        layout = { x: width / 2, y: 0, textAlign: 'center' };
+        break;
+      default:
+        break;
+    }
+
+    return {
+      ...restStyle,
+      ...layout,
+      text: formatter(content),
+    };
+  }
+
+  /**
+   * 创建图例标题
+   */
+  private createTitle() {
+    this.titleShape = new Text({
+      name: 'title',
+      attrs: this.getTitleAttrs(),
+    });
+    this.appendChild(this.titleShape);
+  }
 }
