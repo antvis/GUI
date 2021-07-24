31770f60e3cf6cfcfec4d427f6829f834529bbba
Aarebecca <yangtao.yangtao@alibaba-inc.com>
2021-07-24 10:30:31 +0800

    refactor(legend-continuous): 提供了连续图例，支持size、colro模式，chunk分块

--- src/ui/legend/base.ts
+++ src/ui/legend/base.ts
@@ -1,139 +1,136 @@
-import { deepMix, get, isNumber } from '@antv/util';
+import { deepMix, get } from '@antv/util';
 import { Rect, Text } from '@antv/g';
 import { GUI } from '../core/gui';
-import type { LegendBaseCfg, LegendBaseOptions, StyleStatus } from './types';
+import { getStateStyle, normalPadding } from '../../util';
+import { LEGEND_BASE_DEFAULT_OPTIONS } from './constant';
+import { getShapeSpace } from './utils';
+import type { Pair } from '../slider/types';
+import type { StyleState } from '../../types';
+import type { LegendBaseCfg, LegendBaseOptions } from './types';
 
-export default abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T> {
+export abstract class LegendBase<T extends LegendBaseCfg> extends GUI<T> {
   public static tag = 'legendBase';
 
   // background
-  private backgroundShape: Rect;
+  protected backgroundShape: Rect;
 
   private titleShape: Text;
 
   protected static defaultOptions = {
     type: LegendBase.tag,
-    attrs: {
-      width: 200,
-      height: 40,
-      padding: 0,
-      orient: 'horizontal',
-      indicator: false,
-      backgroundStyle: {
-        fill: '#dcdee2',
-        lineWidth: 0,
-      },
-      title: {
-        content: '',
-        spacing: 10,
-        style: {
-          fontSize: 16,
-          align: 'left',
-          textBaseline: 'top',
-        },
-        formatter: (text: string) => text,
-      },
-    },
+    ...LEGEND_BASE_DEFAULT_OPTIONS,
   };
 
   constructor(options: LegendBaseOptions) {
     super(deepMix({}, LegendBase.defaultOptions, options));
-    this.init();
   }
 
   attributeChangedCallback(name: string, value: any) {}
 
   public init() {
-    this.createBackground();
+    // this.createBackground();
     this.createTitle();
   }
 
+  /**
+   * 获取颜色
+   */
+  protected abstract getColor(): string;
+
   // 获取对应状态的样式
-  private getStyle(name: string | string[], status?: StyleStatus) {
-    const { active, disabled, checked, ...args } = get(this.attributes, name);
-    // 返回默认样式
-    if (!status) return args;
-    return get(this.attributes, [name, status]) || {};
+  protected getStyle(name: string | string[], state?: StyleState) {
+    const style = get(this.attributes, name);
+    return getStateStyle(style, state);
   }
 
   // 获取padding
-  private getPadding() {
-    const { padding } = this.attributes;
-    if (isNumber(padding)) {
-      return new Array(4).fill(padding);
-    }
-    return padding;
+  protected getPadding(padding = this.attributes.padding) {
+    return normalPadding(padding);
   }
 
-  // 获取容器内可用空间
-  private getAvailableSpace() {
+  // 获取容器内可用空间, 排除掉title的空间
+  protected getAvailableSpace() {
+    // 连续图例不固定外部大小
     // 容器大小 - padding - title
-    const { width, height, title } = this.attributes;
-    const { fontSize: lineHeight, spacing } = title;
-    const [top, right, bottom, left] = this.getPadding();
+    const { title } = this.attributes;
+    const { spacing } = title;
+    const [top, , , left] = this.getPadding();
+    const { height: titleHeight } = getShapeSpace(this.titleShape);
+
     return {
       x: left,
-      y: top + lineHeight + spacing,
-      width: width - (left + right),
-      height: height - (top + bottom) - lineHeight - spacing,
+      y: top + titleHeight + spacing,
     };
   }
 
-  private getBackgroundAttrs() {
-    return {
-      ...this.getAvailableSpace(),
-      ...this.getStyle('backgroundStyle'),
-    };
+  /**
+   * 根据方向取值
+   */
+  public getOrientVal<T>([x, y]: Pair<T>) {
+    const { orient } = this.attributes;
+    return orient === 'horizontal' ? x : y;
   }
+
+  /**
+   * 背景属性
+   */
+  protected abstract getBackgroundAttrs();
 
   // 绘制背景
-  private createBackground() {
+  protected createBackground() {
     this.backgroundShape = new Rect({
       name: 'background',
       attrs: this.getBackgroundAttrs(),
     });
-    this.backgroundShape.toBack();
     this.appendChild(this.backgroundShape);
+    this.backgroundShape.toBack();
+  }
+
+  /**
+   * 创建图例标题
+   */
+  protected createTitle() {
+    this.titleShape = new Text({
+      name: 'title',
+      attrs: this.getTitleAttrs(),
+    });
+
+    this.appendChild(this.titleShape);
+  }
+
+  protected adjustTitle() {
+    const { title } = this.attributes;
+    const { width } = getShapeSpace(this);
+    const { align } = title;
+    const [top, right, , left] = this.getPadding();
+
+    let layout: Object;
+    switch (align) {
+      case 'left':
+        layout = { x: left, y: top, textAlign: 'left' };
+        break;
+      case 'right':
+        layout = { x: width - left - right, y: top, textAlign: 'end' };
+        break;
+      case 'center':
+        layout = { x: (width - left - right) / 2, y: top, textAlign: 'center' };
+        break;
+      default:
+        break;
+    }
+    this.titleShape.attr(layout);
   }
 
   /**
    * 创建图例标题配置
    */
   private getTitleAttrs() {
-    const { width, title } = this.attributes;
+    const { title } = this.attributes;
     const { content, style, formatter } = title;
-    const { align, ...restStyle } = style;
-
-    let layout: Object;
-    switch (align) {
-      case 'left':
-        layout = { x: 0, y: 0, textAlign: 'left' };
-        break;
-      case 'right':
-        layout = { x: width, y: 0, textAlign: 'end' };
-        break;
-      case 'center':
-        layout = { x: width / 2, y: 0, textAlign: 'center' };
-        break;
-      default:
-        break;
-    }
 
     return {
-      ...restStyle,
-      ...layout,
+      ...style,
       text: formatter(content),
     };
-  }
-
-  /**
-   * 创建图例标题
-   */
-  private createTitle() {
-    this.titleShape = new Text({
-      name: 'title',
-      attrs: this.getTitleAttrs(),
-    });
-    this.appendChild(this.titleShape);
   }
 }
