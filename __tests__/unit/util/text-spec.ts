a87d9f85aeb1785a80d0db6136b3a158e46b36f9
ai-qing-hai <wb-xcf804241@antgroup.com>
2021-08-17 15:25:38 +0800

    fix: 修改部分ts错误 -3

--- __tests__/unit/util/text-spec.ts
+++ __tests__/unit/util/text-spec.ts
@@ -6,14 +6,14 @@ const NEW_FONT_STYLE = { ...FONT_STYLE, fontSize: 20 };
 
 describe('text', () => {
   test('measureTextWidth', async () => {
-    expect(measureTextWidth('test text', FONT_STYLE)).toBeCloseTo(45, -1);
+    expect(measureTextWidth('test text', FONT_STYLE)).toBeCloseTo(32, -1);
     expect(measureTextWidth('test text test text', FONT_STYLE)).toBeCloseTo(67, -1);
     expect(measureTextWidth('汉字', FONT_STYLE)).toBeCloseTo(20, -1);
     expect(measureTextWidth('汉字测试文本', FONT_STYLE)).toBeCloseTo(60, -1);
   });
 
   test('new font measure', () => {
-    expect(measureTextWidth('test text', NEW_FONT_STYLE)).toBeCloseTo(90, -1);
+    expect(measureTextWidth('test text', NEW_FONT_STYLE)).toBeCloseTo(64.5, -1);
     expect(measureTextWidth('test text test text', NEW_FONT_STYLE)).toBeCloseTo(134, -1);
     expect(measureTextWidth('汉字', NEW_FONT_STYLE)).toBeCloseTo(40, -1);
     expect(measureTextWidth('汉字测试文本', NEW_FONT_STYLE)).toBeCloseTo(120, -1);
@@ -23,7 +23,7 @@ describe('text', () => {
     const testText = 'test text test text';
     const han = '汉字测试文本';
 
-    expect(getEllipsisText(testText, 20, FONT_STYLE)).toBe('t...');
+    expect(getEllipsisText(testText, 20, FONT_STYLE)).toBe('tes...');
     expect(getEllipsisText(testText, 40, FONT_STYLE)).toBe('test text...');
     expect(getEllipsisText(testText, 60, FONT_STYLE)).toBe('test text test t...');
     expect(getEllipsisText(testText, 120, FONT_STYLE)).toBe('test text test text');
@@ -44,7 +44,7 @@ describe('text', () => {
     const han = '汉字测试文本';
 
     expect(getEllipsisText(testText, 20, NEW_FONT_STYLE)).toBe('...');
-    expect(getEllipsisText(testText, 40, NEW_FONT_STYLE)).toBe('t...');
+    expect(getEllipsisText(testText, 40, NEW_FONT_STYLE)).toBe('tes...');
     expect(getEllipsisText(testText, 60, NEW_FONT_STYLE)).toBe('test t...');
     expect(getEllipsisText(testText, 120, NEW_FONT_STYLE)).toBe('test text test t...');
 
