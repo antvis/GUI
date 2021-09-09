06209f867aa685c93f0c744df06cc3d8e83c7d79
Aarebecca <yoonagoddess@163.com>
2021-09-09 15:26:59 +0800

    refactor(crosshair): 为polygon添加了dat.GUI

--- examples/chart-ui/crosshair/demo/polygon.ts
+++ examples/chart-ui/crosshair/demo/polygon.ts
@@ -1,6 +1,7 @@
 import { Canvas } from '@antv/g';
 import { Renderer as CanvasRenderer } from '@antv/g-canvas';
 import { PolygonCrosshair } from '@antv/gui';
+import * as dat from 'dat.gui';
 
 const renderer = new CanvasRenderer({
   enableDirtyRectangleRenderingDebug: false,
@@ -16,8 +17,6 @@ const canvas = new Canvas({
 });
 
 const [cx, cy] = [250, 250];
-const t1 = Math.cos(Math.PI / 6) * 50;
-
 const polygon = new PolygonCrosshair({
   style: {
     defaultRadius: 50,
@@ -30,6 +29,33 @@ const polygon = new PolygonCrosshair({
 });
 canvas.appendChild(polygon);
 
-canvas.addEventListener('mousemove', (e) => {
-  polygon.setPointer([e.offsetX, e.offsetY]);
-});
+// canvas.addEventListener('mousemove', (e) => {
+//   polygon.setPointer([e.offsetX, e.offsetY]);
+// });
+
+const $wrapper = document.getElementById('container');
+const cfg = new dat.GUI({ autoPlace: false });
+$wrapper.appendChild(cfg.domElement);
+const cfgFolder = cfg.addFolder('配置');
+cfgFolder.open();
+
+cfg
+  .add({ sides: 8 }, 'sides', 3, 12)
+  .step(1)
+  .onChange((sides) => {
+    polygon.update({ sides });
+  });
+
+cfg
+  .add({ startAngle: 0 }, 'startAngle', 0, 360)
+  .step(1)
+  .onChange((startAngle) => {
+    polygon.update({ startAngle });
+  });
+
+cfg
+  .add({ defaultRadius: 50 }, 'defaultRadius', 0, 200)
+  .step(5)
+  .onChange((defaultRadius) => {
+    polygon.update({ defaultRadius });
+  });
