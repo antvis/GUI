61ff1f6cf3365326fb678824671b85792f293b9d
Aarebecca <yoonagoddess@163.com>
2021-09-09 14:26:51 +0800

    refactor(util): 简化了throttle

--- src/util/throttle.ts
+++ src/util/throttle.ts
@@ -1,22 +1,19 @@
+/**
+ * 节流修饰器
+ * @param limit 节流时间
+ */
 export function throttle(limit: number = 0) {
-  let flag = true;
+  let prev = new Date().getTime();
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     let func = descriptor.value;
-    func = async (...args: any[]) => {
-      if (!flag) return;
-      flag = false;
-      try {
-        func.apply(target, ...args);
-      } catch (error) {
-        console.log(error);
-      }
-      if (!limit) {
-        flag = true;
-        return;
-      }
-      setTimeout(() => {
-        flag = true;
-      }, limit);
-    };
+    if (typeof func === 'function') {
+      func = (...args: any[]) => {
+        const now = new Date().getTime();
+        if (now - prev > limit) {
+          func.apply(target, args);
+          prev = new Date().getTime();
+        }
+      };
+    }
   };
 }
