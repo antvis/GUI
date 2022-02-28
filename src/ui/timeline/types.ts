dbedce5f10e206916451fe36fb87d690dcdcbd93
jihaina <1748420480@qq.com>
2022-02-28 20:19:31 +0800

    feat: axis

--- src/ui/timeline/types.ts
+++ src/ui/timeline/types.ts
@@ -1,1 +1,211 @@
-export type TimelineOptions = {};
+import { DisplayObjectConfig } from '@antv/g';
+import { CircleProps, MixAttrs, RectProps } from 'types';
+import { CheckboxOptions, LinearOptions } from 'ui';
+import { ButtonCfg } from 'ui/button';
+import { TooltipOptions } from 'ui/tooltip';
+
+type TimeData = {
+  date: string;
+  [k: string]: any;
+};
+export type PlayAxisBaseCfg = {
+  /**
+   * @title x 坐标
+   * @description x 坐标
+   */
+  x: number;
+  /**
+   * @title y 坐标
+   * @description y 坐标
+   */
+  y: number;
+  /**
+   * @title 总长度
+   * @description 整个轴组件的长度
+   */
+  length: number;
+  /**
+   * @title 刻度配置
+   * @description 调整刻度配置,实际上刻度是一条width为0的linear型axis组件
+   */
+  tickOptions: LinearOptions;
+  /**
+   * @title  时间数据
+   * @description 时间数据，必须是均匀等间距的，在方格形的时间轴中每个数据点代表一个小方格
+   */
+  timeData: TimeData[];
+  /**
+   * @title  tooltip内容
+   * @description 自定义tooltip内容
+   */
+  customTooltip?: (time: string | Date) => TooltipOptions;
+  /**
+   * @title  变化时回调函数
+   * @description 监听时间范围（或单一时间）变化的回调函数
+   */
+  onSelectionChange?: (selection: string[] | Date[]) => void;
+};
+
+export type CellAxisCfg = PlayAxisBaseCfg & {
+  /**
+   * @title  cell 样式
+   * @description 格子样式
+   */
+  cell?: MixAttrs<Partial<RectProps>>;
+  /**
+   * @title  背景样式
+   * @description 背景样式
+   */
+  background: RectProps;
+  /**
+   * @title   padding
+   * @description background 的 padding
+   */
+  padding: [number, number, number, number];
+  /**
+   * @title   格子间距
+   * @description 格子间的间距
+   */
+  cellGap: number;
+};
+
+export type SliderAxisCfg = PlayAxisBaseCfg & {
+  /**
+   * @title  手柄样式
+   * @description 手柄样式
+   */
+  handler?: CircleProps;
+  /**
+   * @title  背景样式
+   * @description 背景样式
+   */
+  background?: RectProps;
+  /**
+   * @title  selection样式
+   * @description 选中时间范围样式
+   */
+  selection?: RectProps;
+};
+export type CellAxisOptions = DisplayObjectConfig<CellAxisCfg>;
+export type SliderAxisOptions = DisplayObjectConfig<SliderAxisCfg>;
+type TicksOptions = LinearOptions;
+type SingleModeControl = false | CheckboxOptions;
+type Orient = {
+  layout: 'row' | 'col';
+  controlButtonAlign: 'normal' | 'left' | 'right';
+};
+type Controls =
+  | false
+  | {
+      /**
+       * @title  是否显示单一时间checkbox
+       * @description false 不显示，否则应传入checkbox参数
+       */
+      singleModeControl?: SingleModeControl;
+      /**
+       * @title 播放器按钮，包含：play button，prev button and next button
+       * @description 播放器按钮设置。设置为 null 时，不展示播放器按钮
+       */
+      controlButton?: {
+        /**
+         * @title  停止按钮
+         * @description 播放按钮设置。设置为 null 时，不展示播放按钮
+         */
+        stopBtn?: ButtonCfg;
+        /**
+         * @title 播放按钮
+         * @description 播放按钮设置。设置为 null 时，不展示播放按钮
+         */
+        playBtn?: ButtonCfg;
+        /**
+         * @title 后退按钮
+         * @description 后退按钮设置。设置为 null 时，不展示后退按钮
+         */
+        prevBtn?: ButtonCfg;
+        /**
+         * @title 前进按钮
+         * @description 前进按钮设置。设置为 null 时，不展示前进按钮
+         */
+        nextBtn?: ButtonCfg;
+      };
+      /**
+       * @title 倍速调节器
+       * @description 倍速调节器设置。设置为 null 时，不展示倍速调节器
+       */
+      speedControl?: {
+        /**
+         * @title 可调节的速度
+         * @description 配置可调节的速度，建议配置范围在 5 个区间，如: [1.0, 2.0, 3.0, 4.0, 5.0], [0.5, 1.0, 1.5, 2.0, 2.5]
+         */
+        speeds?: number[];
+        /**
+         * @title   速度变化回调函数
+         * @description 监听速度变化的回调函数
+         */
+        onSpeedChange?: (speed: number) => void;
+      };
+    };
+
+type TooltipFormatter = (time: string | Date) => TooltipOptions;
+
+export type TimelineCfg = {
+  /**
+   * @title x 坐标
+   * @description x 坐标
+   */
+  x: number;
+  /**
+   * @title y 坐标
+   * @description y 坐标
+   */
+  y: number;
+  /**
+   * @title 总宽度
+   * @description 整个组件的宽度
+   */
+  width: number;
+  /**
+   * @title 高度
+   * @description 整个组件的高度
+   */
+  height: number;
+  /**
+   * @title 时间数据
+   * @description 时间数据，必须是均匀等间距的，每个数据点代表一个小方格
+   */
+  data: TimeData[];
+  /**
+   * @title  布局方向
+   * @description 布局方向，见设计稿
+   * @default 'left'
+   */
+  orient?: Orient;
+  /**
+   * @title 播放轴类型
+   * @description 播放轴为slider型还是格子刻度型
+   * @default 'slider'
+   */
+  type: 'slider' | 'cell';
+  /**
+   * @title 播放控制
+   * @description 配置播放器、单一时间checkbox
+   * @default 'slider'
+   */
+  controls?: Controls;
+  /**
+   * @title  tooltip内容
+   * @description 自定义tooltip内容
+   */
+  tooltip?: false | TooltipFormatter;
+  /**
+   * @title   刻度尺
+   * @description 自定义刻度尺
+   */
+  ticks?: false | TicksOptions;
+  /**
+   * @title  变化时回调函数
+   * @description 监听时间范围（或单一时间）变化的回调函数
+   */
+  onSelectionChange?: (selection: string[] | Date[]) => void;
+};
+export type TimelineOptions = DisplayObjectConfig<TimelineCfg>;
