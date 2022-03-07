872a7b61df6a32da49fd7218ec4c7dcf584b4702
jihaina <1748420480@qq.com>
2022-03-07 20:10:18 +0800

    chore: drag bad performance

--- __tests__/unit/ui/timeline/axis-spec.ts
+++ __tests__/unit/ui/timeline/axis-spec.ts
@@ -1,6 +1,7 @@
 import { Canvas } from '@antv/g';
-import { Renderer as CanvasRenderer } from '@antv/g-svg';
-import { CellAxis } from '../../../../src/ui/timeline/playaxis';
+import { Renderer as CanvasRenderer } from '@antv/g-canvas';
+import { SliderAxis } from '../../../../src/ui/timeline/slideraxis';
+import { CellAxis } from '../../../../src/ui/timeline/cellaxis';
 import { createDiv } from '../../../utils';
 
 const renderer = new CanvasRenderer({
@@ -18,22 +19,36 @@ const canvas = new Canvas({
 });
 // 2022年1月的日期数据
 const date = new Array(20).fill(undefined).map((_, id) => ({ date: new Date(2022, 0, id).toLocaleString('zh-CN') }));
+// 2021年1月的日期数据
+const date2 = new Array(15).fill(undefined).map((_, id) => ({ date: new Date(2021, 0, id).toLocaleString('zh-CN') }));
 describe('play axis', () => {
-  test('basic', () => {
+  test('slider', () => {
+    const slideraxis = new SliderAxis({
+      style: {
+        x: 5,
+        y: 60,
+        length: 300,
+        timeData: date,
+        tickCfg: {},
+        selection: [date[0], date[5]],
+      },
+    });
+    slideraxis.update({ timeData: date2, selection: [date2[1], date2[6]] });
+    canvas.appendChild(slideraxis);
+    expect(2).toBe(2);
+  });
+  test('cell', () => {
     const cellaxis = new CellAxis({
       style: {
         x: 20,
         y: 20,
         length: 300,
         timeData: date,
-        tickOptions: {
-          style: {
-            ticksThreshold: 4,
-          },
-        },
+        tickCfg: {},
       },
     });
-
+    cellaxis.update({ timeData: date2 });
+    // console.log(cellaxis.background);
     canvas.appendChild(cellaxis);
     expect(1).toBe(1);
   });
