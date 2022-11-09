d6378fdf1e0531bcad7c0cb3e33f30f4802a3e7e
ChuShen <>
2022-11-09 14:54:23 +0800

    test: config integration test env

---
+++ __tests__/integration/canvas.ts
@@ -0,0 +1,68 @@
+import type { DisplayObject } from '@antv/g';
+import { Canvas } from '@antv/g';
+import { Renderer } from '@antv/g-canvas';
+import { createCanvas } from 'canvas';
+import * as fs from 'fs';
+import pixelmatch from 'pixelmatch';
+import { PNG } from 'pngjs';
+
+/**
+ * diff between PNGs
+ */
+export function diff(src: string, target: string) {
+  const img1 = PNG.sync.read(fs.readFileSync(src));
+  const img2 = PNG.sync.read(fs.readFileSync(target));
+  const { width, height } = img1;
+  return pixelmatch(img1.data, img2.data, null, width, height, {
+    threshold: 0.1,
+  });
+}
+
+export function createGCanvas(width: number, height: number) {
+  // Create a node-canvas instead of HTMLCanvasElement
+  const nodeCanvas = createCanvas(width, height);
+  // A standalone offscreen canvas for text metrics
+  const offscreenNodeCanvas = createCanvas(1, 1);
+
+  // Create a renderer, unregister plugin relative to DOM.
+  const renderer = new Renderer();
+  const domInteractionPlugin = renderer.getPlugin('dom-interaction');
+  renderer.unregisterPlugin(domInteractionPlugin);
+
+  return [
+    new Canvas({
+      width,
+      height,
+      canvas: nodeCanvas as any,
+      renderer,
+      offscreenCanvas: offscreenNodeCanvas as any,
+    }),
+    nodeCanvas,
+  ] as const;
+}
+
+export function sleep(n: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, n);
+  });
+}
+
+export function writePNG(nodeCanvas: any, path: string) {
+  return new Promise<void>((resolve, reject) => {
+    const out = fs.createWriteStream(path);
+    const stream = nodeCanvas.createPNGStream();
+    stream.pipe(out);
+    out.on('finish', resolve).on('error', reject);
+  });
+}
+
+export async function renderCanvas(gshape: DisplayObject, filename: string, defaultWidth = 640, defaultHeight = 480) {
+  const [canvas, nodeCanvas] = createGCanvas(defaultWidth, defaultHeight);
+
+  canvas.appendChild(gshape);
+
+  // Wait for the next tick.
+  await sleep(20);
+  await writePNG(nodeCanvas, filename);
+  return canvas;
+}
--- src/ui/axis/axis.ts
+++ src/ui/axis/axis.ts
@@ -1,6 +1,5 @@
-import { getStylesFromPrefixed, ifShow, sampling, select, filterTransform } from '@/util';
-import { createComponent } from '@/util/create';
 import { DisplayObjectConfig } from '@antv/g';
+import { getStylesFromPrefixed, ifShow, sampling, select, filterTransform, createComponent } from '../../util';
 import { AXIS_BASE_DEFAULT_CFG, CLASS_NAMES } from './constant';
 import { renderGrid } from './guides/axisGrid';
 import { renderLabels } from './guides/axisLabels';
