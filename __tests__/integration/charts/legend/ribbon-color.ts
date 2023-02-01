b5f08eba68b44bd9d1e9c184508409d5859330b8
Aaron <yoonagoddess@163.com>
2023-02-01 21:27:51 +0800

    fix: fix vertical continuous legend with mistake label direction

--- __tests__/integration/charts/legend/ribbon-color.ts
+++ __tests__/integration/charts/legend/ribbon-color.ts
@@ -4,7 +4,7 @@ import { Ribbon } from '../../../../src/ui/legend/continuous/ribbon';
 export const RibbonColor = () => {
   const group = new Group({
     style: {
-      width: 200,
+      width: 410,
       height: 210,
     },
   });
@@ -36,6 +36,25 @@ export const RibbonColor = () => {
   createRibbon({ y: 150, color: ['rgb(255, 0, 0)', '#0f0', 'rgb(0, 0, 255)'] });
 
   createRibbon({ y: 180, color: ['rgba(255, 0, 0, 0)', 'rgba(0, 255, 0, 0.5)', 'rgba(0, 0, 255, 1)'] });
+
+  createRibbon({ x: 200, len: 210, orient: 'vertical', color: ['#f00', '#0f0', '#00f'] });
+
+  createRibbon({ x: 230, len: 210, orient: 'vertical', color: ['red', 'green', 'blue'] });
+
+  createRibbon({ x: 260, len: 210, orient: 'vertical', color: ['gray', 'lightgray'] });
+
+  createRibbon({ x: 290, len: 210, orient: 'vertical', color: ['pink'] });
+
+  createRibbon({ x: 320, len: 210, orient: 'vertical', color: ['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)'] });
+
+  createRibbon({ x: 350, len: 210, orient: 'vertical', color: ['rgb(255, 0, 0)', '#0f0', 'rgb(0, 0, 255)'] });
+
+  createRibbon({
+    x: 380,
+    len: 210,
+    orient: 'vertical',
+    color: ['rgba(255, 0, 0, 0)', 'rgba(0, 255, 0, 0.5)', 'rgba(0, 0, 255, 1)'],
+  });
 
   return group;
 };
