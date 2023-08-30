d69ecebb0a62adf77d6a3bec173dbad89847b400
Chushen <yangtao.yangtao@antgroup.com>
2023-08-30 19:50:52 +0800

    refactor: refactor time util

--- src/ui/timebar/utils.ts
+++ src/ui/timebar/utils.ts
@@ -1,4 +1,5 @@
 import type { Interval } from './types';
+import { formatTime } from '../../util/time';
 // TODO 需要根据传入时间、当前时间轴宽度 自动计算出最佳的时间间隔
 /**
  * 时间格式化，为 Date 时，按照时间解析，为数字时按照序列解析
@@ -15,13 +16,8 @@ export function labelFormatter(time: number | Date, interval?: Interval) {
   return parseByTime(time, interval!);
 }
 
-function parseByTime(time: Date, interval: Interval) {
+export function parseByTime(time: Date, interval: Interval) {
   const date = new Date(time);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
 
   switch (interval) {
     case 'half-hour':
@@ -29,51 +25,51 @@ function parseByTime(time: Date, interval: Interval) {
     case 'four-hour':
       if ([0, 6, 12, 18].includes(date.getHours()) && date.getMinutes() === 0) {
         // HH:mm\nYYYY-MM-DD
-        return `${hours}:${minutes}\n${year}-${month}-${day}`;
+        return formatTime(date, 'HH:mm\nYYYY-MM-DD');
       }
       // HH:mm
-      return `${hours}:${minutes}`;
+      return formatTime(date, 'HH:mm');
     case 'half-day':
       // A\nYYYY-MM-DD
       if (date.getHours() < 12) {
-        return `AM\n${year}-${month}-${day}`;
+        return `AM\n${formatTime(date, 'YYYY-MM-DD')}`;
       }
       // A
       return 'PM';
     case 'day':
       if ([1, 10, 20].includes(date.getDate())) {
         // DD\nYYYY-MM
-        return `${day}\n${year}-${month}`;
+        return formatTime(date, 'DD\nYYYY-MM');
       }
       // DD
-      return day;
+      return formatTime(date, 'DD');
     case 'week':
       if (date.getDate() <= 7) {
         // DD\nYYYY-MM
-        return `${day}\n${year}-${month}`;
+        return formatTime(date, 'DD\nYYYY-MM');
       }
       // DD
-      return day;
+      return formatTime(date, 'DD');
     case 'month':
       if ([0, 6].includes(date.getMonth())) {
         // MM月\nYYYY
-        return `${month}月\n${year}`;
+        return formatTime(date, 'MM月\nYYYY');
       }
       // MM月
-      return `${month}月`;
+      return formatTime(date, 'MM月');
     case 'season':
       if ([0].includes(date.getMonth())) {
         // MM月\nYYYY
-        return `${month}月\n${year}`;
+        return formatTime(date, 'MM月\nYYYY');
       }
       // MM月
-      return `${month}月`;
+      return formatTime(date, 'MM月');
     case 'year':
       // YYYY
-      return `${year}`;
+      return formatTime(date, 'YYYY');
     default:
       // YYYY-MM-DD HH:mm
-      return `${year}-${month}-${day} ${hours}:${minutes}`;
+      return formatTime(date, 'YYYY-MM-DD HH:mm');
   }
 }
 
@@ -82,7 +78,7 @@ function parseByTime(time: Date, interval: Interval) {
  * @param time
  * @param interval
  */
-function parseBySeries(time: number) {
+export function parseBySeries(time: number) {
   const hours = String(Math.floor(time / 3600)).padStart(2, '0');
   const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0');
   const seconds = String(Math.floor(time % 60)).padStart(2, '0');
